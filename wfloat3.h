3126953fc945bdd1c89a487ae2c9e0d0cd46f676
Kelly Rowland <kellylynnerowland@gmail.com>
2014-06-20 12:59:52 -0700

    Doxygen comments

--- wfloat3.h
+++ wfloat3.h
@@ -1,24 +1,109 @@
 #ifndef WFLOAT3_H
 #define WFLOAT3_H
 
-//class definitions for device vector operations
+/**
+ * \class wfloat3 wfloat3.h
+ * \brief class definitions for device vector operations
+ */
+
 class wfloat3{
 public:
-	float x,y,z;
-	inline __device__ 			wfloat3();
-	inline __device__ 			wfloat3(float);
-	inline __device__ 			wfloat3(float,float,float);
+	float x; /**< x-coordinate */
+	float y; /**< y-coordinate */
+	float z; /**< z-coordinate */
+	/**
+	 * \brief sets x,y,z to 0,0,0
+	 */
+	inline __device__ 		wfloat3();
+	/**
+	 * \brief sets x,y,z to a,a,a
+	 * @param[in] a - point to set
+	 */ 
+	inline __device__ 		wfloat3(float);
+	/**
+	 * \brief sets x,y,z to a,b,c
+	 * @param[in] a,b,c - points to set
+	 */
+	inline __device__ 		wfloat3(float,float,float);
+	/**
+	 * \brief vector addition operator
+	 * \details adds x and x-component of input wfloat3, etc.
+	 * @param[in] arg - wfloat3 coordinates to be added
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator+ (wfloat3); 
+	/**
+	 * \brief vector subtraction operator
+	 * \details subtracts x-component of input wfloat3 from x, etc.
+	 * @param[in] arg - wfloat3 coordinates to be subtracted
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator- (wfloat3);
+	/**
+	 * \brief vector multiplication operator
+	 * \details multiplies x and x-component of input wfloat3, etc.
+	 * @param[in] arg - wfloat3 coordinates to be multiplied
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator* (wfloat3);
+	/**
+	 * \brief scalar addition operator
+	 * \details adds x and arg, etc.
+	 * @param[in] arg - number to be added
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator+ (float); 
+	/**
+	 * \brief scalar subtraction operator
+	 * \details subtracts arg from x, etc.
+	 * @param[in] arg - number to be subtracted
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator- (float);
+	/**
+	 * \brief scalar multiplication operator
+	 * \details multiplies x and arg, etc.
+	 * @param[in] arg - number by which to multiply
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator* (float);
+	/**
+	 * \brief scalar divison operator
+	 * \details divides x by arg, etc.
+	 * @param[in] arg - number by which to divide
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	operator/ (float);
+	/**
+	 * \brief cross product operator
+	 * \details returns the cross product of the vector and arg
+	 * @param[in] arg - vector to cross
+	 * \returns result - resultant wfloat3
+	 */
 	inline __device__ wfloat3 	cross(wfloat3);
+	/**
+	 * \brief dot product operator
+	 * \details returns the dot product of the vector and arg
+	 * @param[in] arg - vector to dot
+	 */
 	inline __device__ float   	dot(wfloat3);
+	/**
+	 * \brief Rodrigues' rotation operator
+	 * \details rotates a vector in space, given axis and angle of rotation
+	 * @param[in]  k - unit vector describing axis of rotation about which to rotate
+	 * @param[in] theta - angle by which to rotate
+	 */
 	inline __device__ void  	rodrigues_rotation(wfloat3,float);
+	/**
+	 * \brief rotation about random cosine
+	 * \details borrowed from OpenMC
+	 * @param[in] mu - random cos(theta)
+	 * @param[in] rn - random number
+	 */ 
 	inline __device__ wfloat3  	rotate(float,float);
+	/**
+	 * \brief returns square root of sum of squares of coordinates
+	 */
 	inline __device__ float 	norm2();
 };
 __device__ wfloat3::wfloat3(){x=0;y=0;z=0;};
