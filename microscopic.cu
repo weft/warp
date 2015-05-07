1ac3b13ef18a94b785a141a4698f1e27b6d5183f
Ryan M. Bergmann <ryanmbergmann@gmail.com>
2015-05-07 15:44:36 -0400

    mt4 temp fix, change to absorbtion

--- microscopic.cu
+++ microscopic.cu
@@ -102,8 +102,8 @@ __global__ void microscopic_kernel(unsigned N, unsigned n_isotopes, unsigned n_c
 	//if( this_rxn >= 811 & this_rxn<850 & this_rxn!=818 ){printf("microscopic sampled tid %u rxn %d energy %6.4E\n",tid,this_rxn,this_E);}
 	//printf("%u\n",this_rxn);
 	if(this_rxn == 3 | this_rxn==4 | this_rxn ==5 | this_rxn ==10 | this_rxn ==27){
-		this_rxn = 1102;
 		printf("MT=%u!!!, changing to 1102...\n",this_rxn);
+		this_rxn = 1102;
 	}
 	//if(this_rxn==56){printf("MT=56 at E %10.8E\n",this_E);}
 	rxn[tid_in] = this_rxn;
