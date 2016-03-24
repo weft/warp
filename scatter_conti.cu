a5f4b66a3b00b5a4d97306d49ae24fbd0e3099f7
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2016-03-24 10:39:48 +0100

    continuum scatter debug

--- scatter_conti.cu
+++ scatter_conti.cu
@@ -355,7 +355,7 @@ __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_
 		printf("continuum scatter mis-sampled!  tid_in %d tid %d E0 %6.4E sampled_E %6.4E dist len %u dist_index %u rn %6.4E var0 %6.4E var1 %6.4E cdf0 %6.4E cdf1 %6.4E pdf0 %6.4E pdf1 %6.4E... \n",tid_in,tid,E0,sampled_E,this_edist.len,dist_index[0],rn2,this_edist.var[dist_index[0]],this_edist.var[dist_index[0]+1],this_edist.cdf[dist_index[0]],this_edist.cdf[dist_index[0]+1],this_edist.pdf[dist_index[0]],this_edist.pdf[dist_index[0]+1]);
 	}
 	if (!isfinite(mu) | mu < -1.0 | mu > 1.0){
-		printf("continuum scatter mis-sampled tid %i data_dex %u mu %6.4E dist len %u dist_index %u... \n",tid_in,tid,mu,this_sdist.len,dist_index[0]);
+		printf("continuum scatter mis-sampled tid_in %d tid %d this_E %6.4E law %u mu %6.4E dist len %u dist_index %u... \n",tid_in,tid,this_e,this_law,mu,this_sdist.len,dist_index[0]);
 	}
 
 
