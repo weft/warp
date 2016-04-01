1208a89ffbd782d1defac21fcdd26390aa7fbea7
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2016-04-01 10:49:31 +0200

    more conti debugging

--- scatter_conti.cu
+++ scatter_conti.cu
@@ -9,9 +9,9 @@
 __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_section_data* d_xsdata, particle_data* d_particles, unsigned* d_remap){
 
 	// declare shared variables
-	__shared__ 	unsigned			n_isotopes;				
-	__shared__ 	unsigned			energy_grid_len;		
-	__shared__ 	unsigned			total_reaction_channels;
+	//__shared__ 	unsigned			n_isotopes;				
+	//__shared__ 	unsigned			energy_grid_len;		
+	//__shared__ 	unsigned			total_reaction_channels;
 	//__shared__ 	unsigned*			rxn_numbers;			
 	//__shared__ 	unsigned*			rxn_numbers_total;		
 	//__shared__ 	float*				energy_grid;			
@@ -35,9 +35,9 @@ __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_
 
 	// have thread 0 of block copy all pointers and static info into shared memory
 	if (threadIdx.x == 0){
-		n_isotopes					= d_xsdata[0].n_isotopes;								
-		energy_grid_len				= d_xsdata[0].energy_grid_len;				
-		total_reaction_channels		= d_xsdata[0].total_reaction_channels;
+		//n_isotopes					= d_xsdata[0].n_isotopes;								
+		//energy_grid_len				= d_xsdata[0].energy_grid_len;				
+		//total_reaction_channels		= d_xsdata[0].total_reaction_channels;
 		//rxn_numbers 				= d_xsdata[0].rxn_numbers;						
 		//rxn_numbers_total			= d_xsdata[0].rxn_numbers_total;					
 		//energy_grid 				= d_xsdata[0].energy_grid;						
@@ -94,7 +94,6 @@ __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_
 	unsigned	rn				=	rn_bank[ tid];
 	float		this_awr		=	awr[ this_tope];
 	//float		this_temp		=	temp[this_tope];
-	unsigned	max_index 		= 	(n_isotopes + total_reaction_channels)*energy_grid_len;
 
 	// pick upper or lower via stochastic mixing
 	dist_data	this_edist, this_sdist;
@@ -105,22 +104,11 @@ __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_
 		printf("scatter_conti: null pointer dist_energy, tid %u\n",tid);
 		return;
 	}
-	else{
-		if(this_dex >= max_index){
-			printf("scatter_conti: this_dex %u > max_index %u!\n",this_dex,max_index);
-			return;
-		}
-		edist_lower	=	dist_energy[this_dex].lower[0];
-		edist_upper	=	dist_energy[this_dex].upper[0];
-	}
+
 	// check S data pointers
 	if(dist_scatter == 0x0){
 		printf("scatter_conti: null pointer dist_scatter, tid %u\n",tid);
 		return;
-	}
-	else{
-		sdist_lower	=	dist_scatter[this_dex].lower[0];
-		sdist_upper	=	dist_scatter[this_dex].upper[0];
 	}
 
 	// check second level pointers
@@ -128,6 +116,12 @@ __global__ void scatter_conti_kernel(unsigned N, unsigned starting_index, cross_
 	if(dist_scatter[this_dex].upper == 0x0){printf("scatter_conti: null pointer dist_scatter.upper! this_dex %u this_E %6.4E tope %u\n",this_dex,this_E,this_tope); return;}
 	if(dist_energy[ this_dex].upper == 0x0){printf("scatter_conti: null pointer dist_energy.upper!  this_dex %u this_E %6.4E tope %u\n",this_dex,this_E,this_tope); return;}
 	if(dist_energy[ this_dex].lower == 0x0){printf("scatter_conti: null pointer dist_energy.lower!  this_dex %u this_E %6.4E tope %u\n",this_dex,this_E,this_tope); return;}
+
+	// load now that verified
+	edist_lower	=	dist_energy[ this_dex].lower[0];
+	edist_upper	=	dist_energy[ this_dex].upper[0];
+	sdist_lower	=	dist_scatter[this_dex].lower[0];
+	sdist_upper	=	dist_scatter[this_dex].upper[0];
 
 	unsigned	this_law;
 	float		f			=	(this_E - edist_lower.erg) / (edist_upper.erg - edist_lower.erg);
